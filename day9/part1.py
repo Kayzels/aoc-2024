0063efa54b398f9c72d74225e5f80b6641ab92ae
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2024-12-15 16:19:58 +0200

    (day9): Update Part 1

--- day9/part1.py
+++ day9/part1.py
@@ -1,29 +1,38 @@
-def to_positions(info: str) -> list[complex]:
+from dataclasses import dataclass
+
+
+@dataclass
+class Position:
+    value: int
+    count: int
+
+
+def to_positions(info: str) -> list[Position]:
     current_val = 0
-    positions: list[complex] = []
+    positions: list[Position] = []
     for index, char in enumerate(info):
         cnt = int(char)
         if index % 2 == 1:
-            positions.append(complex(-1, cnt))
+            positions.append(Position(-1, cnt))
         else:
-            positions.append(complex(current_val, cnt))
+            positions.append(Position(current_val, cnt))
             current_val += 1
     return positions
 
 
-def convert(positions: list[complex]) -> list[complex]:
+def convert(positions: list[Position]) -> list[Position]:
     first_ind: int = 0
     last_ind: int = len(positions) - 1
 
     while first_ind < last_ind:
         first_pos = positions[first_ind]
-        while first_pos.real >= 0:
+        while first_pos.value >= 0:
             first_ind += 1
             first_pos = positions[first_ind]
         # After this, first is the first blank
 
         last_pos = positions[last_ind]
-        while last_pos.imag <= 0 or last_pos.real < 0:
+        while last_pos.count <= 0 or last_pos.value < 0:
             last_ind -= 1
             last_pos = positions[last_ind]
         # After this, last is the last non blank
@@ -31,35 +40,34 @@ def convert(positions: list[complex]) -> list[complex]:
         if first_ind >= last_ind:
             break
 
-        if first_pos.imag <= last_pos.imag:
-            new_val = complex(last_pos.real, first_pos.imag)
-            update_last = complex(last_pos.real, last_pos.imag - first_pos.imag)
-            positions[first_ind] = new_val
-            positions[last_ind] = update_last
+        if first_pos.count <= last_pos.count:
+            positions[first_ind].value = last_pos.value
+            positions[last_ind].count = last_pos.count - first_pos.count
         else:
-            new_val = complex(last_pos.real, last_pos.imag)
-            update_last = complex(last_pos.real, last_pos.imag - first_pos.imag)
-            positions[first_ind] = new_val
+            update_last = Position(last_pos.value, last_pos.count - first_pos.count)
+            positions[first_ind] = last_pos
             positions[last_ind] = update_last
-            positions.insert(first_ind + 1, complex(-1, first_pos.imag - last_pos.imag))
+            positions.insert(
+                first_ind + 1, Position(-1, first_pos.count - last_pos.count)
+            )
 
-    positions = list(filter(lambda pos: pos.real >= 0 and pos.imag > 0, positions))
+    positions = list(filter(lambda pos: pos.value >= 0 and pos.count > 0, positions))
     return positions
 
 
-def calculate(positions: list[complex]) -> int:
+def calculate(positions: list[Position]) -> int:
     index: int = 0
     acc: int = 0
     for position in positions:
-        if position.real < 0:
+        if position.value < 0:
             continue
-        for i in range(int(position.imag)):
-            acc += int(position.real) * (index + i)
-        index += int(position.imag)
+        for i in range(int(position.count)):
+            acc += int(position.value) * (index + i)
+        index += int(position.count)
     return acc
 
 
-with open("./day9/input") as file:
+with open("./day9/test_input") as file:
     info = file.readline().strip()
 
 positions = convert(to_positions(info))
